e83b1367feaf6fc1844ee1390f34bca53a2c3082
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-01-20 19:13:36 -0500

    Update Main.hx

--- sourceJon/Main.hx
+++ sourceJon/Main.hx
@@ -72,8 +72,6 @@ class Main extends Sprite
 		initialState = TitleState;
 		#end
 
-		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
-
 		addChild(game);
 
 		var ourSource:String = "assets/videos/DO NOT DELETE OR GAME WILL CRASH/dontDelete.webm";
